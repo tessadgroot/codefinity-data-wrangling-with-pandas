b53d2ef133e579d8f1853fc6174a3a6e925c02bc
tessadgroot <>
2025-12-11 16:19:51 +0000

    Multi-column grouping

---
+++ aggregating_data/multi_column_grouping/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/4bf24830-59ba-4418-969b-aaf8117d522e/plane', index_col = 0)
+
+# Group data
+data_flights = data[['AirportFrom', 'DayOfWeek','Time']].groupby(['AirportFrom', 'DayOfWeek']).mean()
+
+# Output the first 10 rows of the data set
+print(data_flights.head(10))
